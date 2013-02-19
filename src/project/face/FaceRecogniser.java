979e7ba006d3052e5cfbc1ed426163e7182d5701
kubac65 <kubac65@darkstar-m>
2013-02-19 15:33:50 +0000

    FaceRecogniser, Fixed addPersonMethod, Added saving and reading Trainig data from files.

--- src/project/face/FaceRecogniser.java
+++ src/project/face/FaceRecogniser.java
@@ -4,6 +4,12 @@ import com.googlecode.javacv.cpp.opencv_contrib.FaceRecognizer;
 
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +20,10 @@ import static com.googlecode.javacv.cpp.opencv_highgui.*;
 
 
 public class FaceRecogniser {
-	private final int FACES_PER_PERSON = 10;
+	private final String TR_DATA_FILE = "TRData.xml";
+	private final String TR_NAMES_FILE = "TRNames.bin";
+	
+	private final int FACES_PER_PERSON = 8;
 	private int personLimit = 0;
 	private int personCount = 0;
 	
@@ -27,14 +36,20 @@ public class FaceRecogniser {
 	int trainingImagesC, labelsC = 0;
 	
 	FaceRecogniser() {
-		recogniser = createLBPHFaceRecognizer();
-		recogniser.set("threshold", 85.0);
+		initialSetUp();
+	}
+	FaceRecogniser(String aPath){
+		initialSetUp();
+		loadTrainingDate(aPath);
 	}
 	FaceRecogniser(int aPersonCount){
-		recogniser = createLBPHFaceRecognizer();
-		recogniser.set("threshold", 85.0);
+		initialSetUp();
 		initialiseRecogniser(aPersonCount);
 		
+	}
+	private void initialSetUp(){
+		recogniser = createLBPHFaceRecognizer();
+		recogniser.set("threshold", 85.0);
 	}
 	public boolean isTrained(){
 		return trained;
@@ -48,7 +63,7 @@ public class FaceRecogniser {
 	public void addPerson(String aName, String aPhotoDirectoryPath){
 		names.put(labelsC, aName);
 		for(int i = 0; i < FACES_PER_PERSON; i++){
-			trainingImages.put(trainingImagesC, cvLoadImage(aPhotoDirectoryPath + i + ".jpg"));
+			trainingImages.put(trainingImagesC, cvLoadImage(aPhotoDirectoryPath + i + ".jpg", CV_LOAD_IMAGE_GRAYSCALE));
 			labels[trainingImagesC] = labelsC;
 			trainingImagesC++;
 		}
@@ -72,5 +87,33 @@ public class FaceRecogniser {
 		}
 		return name;
 	}
-	
+	public void saveTrainingData(String aPath){
+		recogniser.save(aPath + TR_DATA_FILE);
+		try {
+			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(aPath + TR_NAMES_FILE));
+			os.writeObject(names);
+			os.close();
+			os.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void loadTrainingDate(String aPath){
+		recogniser.load(aPath + "TR_Data.xml");
+		try {
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream(aPath + TR_NAMES_FILE));
+			names = (Map<Integer, String>) is.readObject();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		trained = true;
+	}
 }
--- src/project/face/Test.java
+++ src/project/face/Test.java
@@ -1,28 +1,143 @@
 package project.face;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+import static com.googlecode.javacv.cpp.opencv_core.*;
+import static com.googlecode.javacv.cpp.opencv_imgproc.*;
+import static com.googlecode.javacv.cpp.opencv_objdetect.*;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+
+import com.googlecode.javacpp.IntPointer;
+import com.googlecode.javacv.cpp.opencv_contrib.*;
+import static com.googlecode.javacv.cpp.opencv_contrib.createEigenFaceRecognizer;
+import static com.googlecode.javacv.cpp.opencv_contrib.createFisherFaceRecognizer;
+import static com.googlecode.javacv.cpp.opencv_contrib.createLBPHFaceRecognizer;
+import com.googlecode.javacv.cpp.opencv_contrib.FaceRecognizer;
+import com.googlecode.javacv.cpp.opencv_core;
+import com.googlecode.javacv.cpp.opencv_core.CvArrArray;
+import com.googlecode.javacv.cpp.opencv_core.MatVector;
+
 import project.face.FaceOperations;
 
 public class Test {
-	
-	/*
-	 * Grrrrrrr!!!! 
-	 */
 
 	  public static void main(String[] args) throws Exception {
-		  BufferedImage img = ImageIO.read(new File("2.jpg"));
+		//extract();
+		//recognise();
+		//recogniseMany(extractq());
+		  
+		FaceRecogniser f = new FaceRecogniser(3);
+		/*f.addPerson("Sam", "./studentImgs/R00072205/");
+		f.addPerson("Jakub", "./studentImgs/R00068252/");
+		f.addPerson("Neil", "./studentImgs/R00076373/");
+		f.train();
+		f.saveTrainingData("./studentImgs/");*/
+		f.loadTrainingDate("./studentImgs/");
+		System.out.println(f.recognise(cvLoadImage("4.jpg", CV_LOAD_IMAGE_GRAYSCALE)));
+	  }
+public static void extract() throws IOException{
+		  BufferedImage img = ImageIO.read(new File("a.jpg"));
 		  ArrayList<FaceFrame> frames = new ArrayList<FaceFrame>();
 		  frames = FaceOperations.getFacesCoords(img);
-		  int i = 20;
+		  int i=20;
 		  for(FaceFrame f: frames){
 			 ImageIO.write(FaceOperations.prepareImageForRecognition(img.getSubimage(f.getA(), f.getB(), f.getWidth(), f.getHeight())), "jpg", new File(i + ".jpg"));
 			 i++;
 		 }
-		ImageIO.write(FaceOperations.markFaces(img, FaceOperations.getFacesCoords(img)),
-				 "jpg", new File("out.jpg"));
-	  }
+	
+}
+public static ArrayList<BufferedImage> extractq() throws IOException{
+	ArrayList<BufferedImage> faces = new ArrayList<BufferedImage>();
+	
+		  BufferedImage img = ImageIO.read(new File("b.jpg"));
+		  ArrayList<FaceFrame> frames = new ArrayList<FaceFrame>();
+		  frames = FaceOperations.getFacesCoords(img);
+		  for(FaceFrame f: frames){
+			 faces.add(FaceOperations.prepareImageForRecognition(img.getSubimage(f.getA(), f.getB(), f.getWidth(), f.getHeight())));
+		 }
+		  return faces;
+}
+	
+	/*
+	  BufferedImage img = ImageIO.read(new File("1.jpg"));
+	  ArrayList<FaceFrame> frames = new ArrayList<FaceFrame>();
+	  frames = FaceOperations.getFacesCoords(img);
+	  int i = 20;
+	  for(FaceFrame f: frames){
+		 ImageIO.write(FaceOperations.prepareImageForRecognition(img.getSubimage(f.getA(), f.getB(), f.getWidth(), f.getHeight())), "jpg", new File(i + ".jpg"));
+		 i++;
+	 }
+  
+	//ImageIO.write(FaceOperations.markFaces(img, FaceOperations.getFacesCoords(img)),
+	//
+}*/
+public static void recognise(){
+		
+		//Local Binary Patterns Histograms recogniser has best performance on small sample spaces.
+		FaceRecognizer f = createLBPHFaceRecognizer();
+		//f.set("threshold", 90.0);
+		
+		int id[] = new int[24];
+		MatVector images = new MatVector(24);
+		
+		int j = 0;
+		
+		for(int i = 1; i < 9; i++){
+			images.put(j, cvLoadImage(i+".jpg", CV_LOAD_IMAGE_GRAYSCALE));
+			id[j]=0;
+			j++;
+		}
+		
+		for(int i = 900; i < 908; i++){
+			images.put(j, cvLoadImage(i+".jpg", CV_LOAD_IMAGE_GRAYSCALE));
+			id[j]=1;
+			j++;
+		}
+		for(int i = 700; i < 708; i++){
+			images.put(j, cvLoadImage(i+".jpg", CV_LOAD_IMAGE_GRAYSCALE));
+			id[j]=2;
+			j++;
+		}
+		
+		f.train(images, id);
+		//f.load("nowy.xml");
+		CvArr input = cvLoadImage("a.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+		int a = f.predict(input);
+		//f.save("nowy.xml");
+		String b;
+		if(a==0){b="Sam";}
+		else if(a==1){b="Jakub";}
+		else if(a==2){b="Neil";}
+		else{b="not recognised.";}
+		System.out.println("Predicted Person: " + b);
+		//System.out.println("Prediction: " + prediction.toString());
+		
+	}
+public static void recogniseMany(ArrayList<BufferedImage>  bi){
+	
+	//Local Binary Patterns Histograms recogniser has best performance on small sample spaces.
+	FaceRecognizer f = createLBPHFaceRecognizer();
+	//f.set("threshold", 100.0);
+	f.load("nowy.xml");
+	
+	for(BufferedImage i : bi){
+		IplImage img = IplImage.createFrom(i);
+		CvArr input = img;
+		int a = f.predict(input);
+		String b;
+		if(a==0){b="Sam";}
+		else if(a==1){b="Jakub";}
+		else if(a==2){b="Neil";}
+		else{b="not recognised.";}
+		System.out.println("Predicted Person: " + b);
+	}
+	
+	//System.out.println("Prediction: " + prediction.toString());
+	
+}
+
 }
